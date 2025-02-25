c59b6595d0b7246a786fc174615d5fe89a88864c
Sergio Monge <82247215+SergioDispla@users.noreply.github.com>
2025-02-24 21:42:06 -0600

    Update change_blackstone.sh

--- change_blackstone.sh
+++ change_blackstone.sh
@@ -25,7 +25,7 @@ chmod 777 /opt/lampp/htdocs/BlackStone/assets/images/auth/pqsecurity.jpg
 sed --in-place '112 c\      Copyright &copy; <?php echo $anioActual; ?> PQ Security. All rights reserved.' /opt/lampp/htdocs/BlackStone/gen_word_tecnico.php
 sed --in-place '150 c\       <img width=320 height=170 id=image1.jpeg src="assets/images/report/logo_portada2.png" style="margin-left:-75px;">' /opt/lampp/htdocs/BlackStone/gen_word_tecnico.php
 sed --in-place "235 c\               <span style=\'font-size:10.0pt;font-family:\"Verdana\",sans-serif\'>PQ Security<span style=\'letter-spacing:-.05pt\'>" /opt/lampp/htdocs/BlackStone/gen_word_tecnico.php
-sed --in-place '275 c\         <?php echo lang("and their systems.");?> PQ Security<?php echo lang("recommends that special precautions be taken to");?>'  /opt/lampp/htdocs/BlackStone/gen_word_tecnico.php
+sed --in-place '275 c\         <?php echo lang("and their systems.");?> PQ Security<?php echo lang(" recommends that special precautions be taken to");?>'  /opt/lampp/htdocs/BlackStone/gen_word_tecnico.php
 sed --in-place '287 c\       <?php echo lang("or maintained by other providers. PQ Security bases these recommendations on of your previous experience with the capabilities of these products. However, PQ Security cannot and should not guarantee that any particular product will perform as advertised by the seller.");?>'  /opt/lampp/htdocs/BlackStone/gen_word_tecnico.php
 sed --in-place '19898 c\  background: url("../images/auth/pqsecurity.jpg");' /opt/lampp/htdocs/BlackStone/assets/css/style.css
 
